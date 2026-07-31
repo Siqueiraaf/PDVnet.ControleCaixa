a49994626b2dfa0207f1220cc920c850cea9ba46
Siqueiraaf <dev.rafaelsiqueira@gmail.com>
2026-07-31 12:47:24 -0300

    Ajusta input de valor no MovimentacaoForm para moeda
    Campo Valor agora manipulado manualmente via eventos, permitindo apenas números e formatação automática como moeda (pt-BR). Atualização do ViewModel em tempo real. Removida validação antiga por Regex e ajustada validação de descrição.

--- PDVnet.ControleCaixa.UI/Views/MovimentacaoForm.xaml.cs
+++ PDVnet.ControleCaixa.UI/Views/MovimentacaoForm.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Text.RegularExpressions;
-using System.Windows;
+﻿using PDVnet.ControleCaixa.UI.ViewModels.Movimentacao;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -12,18 +13,68 @@ public partial class MovimentacaoForm : UserControl
         InitializeComponent();
     }
 
-    private static readonly Regex NumberRegex = new("[^0-9,]+");
-
     private static readonly Regex DescriptionRegex =
         new(@"^[\p{L}\p{N}\p{P}\p{Zs}]+$");
-
-    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
-    {
-        e.Handled = NumberRegex.IsMatch(e.Text);
-    }
 
     private void DescriptionValidationTextBox(object sender, TextCompositionEventArgs e)
     {
         e.Handled = !DescriptionRegex.IsMatch(e.Text);
+    }
+
+    private void ValorTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+
+        if (!char.IsDigit(e.Text[0]))
+        {
+            e.Handled = true;
+            return;
+        }
+
+        string numeros = Regex.Replace(textBox.Text, @"\D", "");
+        numeros += e.Text;
+
+        if (string.IsNullOrEmpty(numeros))
+            numeros = "0";
+
+        decimal valor = decimal.Parse(numeros) / 100m;
+
+        if (DataContext is MovimentacaoFormViewModel vm)
+        {
+            vm.Valor = valor;
+        }
+
+        textBox.Text = valor.ToString("N2", new CultureInfo("pt-BR"));
+        textBox.CaretIndex = textBox.Text.Length;
+
+        e.Handled = true;
+    }
+
+    private void ValorTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+
+        if (e.Key != Key.Back)
+            return;
+
+        string numeros = Regex.Replace(textBox.Text, @"\D", "");
+
+        if (numeros.Length > 0)
+            numeros = numeros[..^1];
+
+        if (string.IsNullOrEmpty(numeros))
+            numeros = "0";
+
+        decimal valor = decimal.Parse(numeros) / 100m;
+
+        if (DataContext is MovimentacaoFormViewModel vm)
+        {
+            vm.Valor = valor;
+        }
+
+        textBox.Text = valor.ToString("N2", new CultureInfo("pt-BR"));
+        textBox.CaretIndex = textBox.Text.Length;
+
+        e.Handled = true;
     }
 }
